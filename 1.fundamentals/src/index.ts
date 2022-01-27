e087b1cbdb08df5195d1851052977e1dcc891d17
Nathan Torquato <ntorquato@voice.com>
2022-01-27 12:15:04 -0300

    feat: create a transaction and submit it to local blockchain

--- 1.fundamentals/src/index.ts
+++ 1.fundamentals/src/index.ts
@@ -1,31 +1,36 @@
 import Web3 from 'web3'
+import { Transaction, TxData } from 'ethereumjs-tx'
 
-const mainnetUrl = 'https://mainnet.infura.io/v3/af3df0d221fc422596bd1ddce10fc709'
-// const rinkebyUrl = 'https://rinkeby.infura.io/v3/af3df0d221fc422596bd1ddce10fc709'
-const web3 = new Web3(mainnetUrl)
-
-const mainnetAccount = '0x742d35Cc6634C0532925a3b844Bc454e4438f44e'
-
-// const rinkebyAccount = '0xa7a82DD06901F29aB14AF63faF3358AD101724A8'
+const localURL = 'http://localhost:7545'
+const web3 = new Web3(localURL)
 
 ;(async () => {
   console.clear()
 
-  const balance = await web3.eth.getBalance(mainnetAccount)
-  const ethBalance = web3.utils.fromWei(balance, 'ether')
-  console.log({ ethBalance })
-  
-  const transactionCount = await web3.eth.getTransactionCount(mainnetAccount)
-  console.log({ transactionCount })
+  const accounts = await web3.eth.getAccounts()
 
-  const contractAddress = '0x0d8775f648430679a709e98d2b0cb6250d2887ef' // BAT
-  const abi = [{"constant":true,"inputs":[],"name":"batFundDeposit","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"batFund","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"tokenExchangeRate","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"finalize","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"version","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"refund","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"tokenCreationCap","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"isFinalized","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"fundingEndBlock","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"ethFundDeposit","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"createTokens","outputs":[],"payable":true,"type":"function"},{"constant":true,"inputs":[],"name":"tokenCreationMin","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"fundingStartBlock","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"_ethFundDeposit","type":"address"},{"name":"_batFundDeposit","type":"address"},{"name":"_fundingStartBlock","type":"uint256"},{"name":"_fundingEndBlock","type":"uint256"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"LogRefund","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"CreateBAT","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Approval","type":"event"}] as any
-  const contract = new web3.eth.Contract(abi, contractAddress)
-  const [ name, symbol, totalSupply ] = await Promise.all([
-    contract.methods.name().call(),
-    contract.methods.symbol().call(),
-    contract.methods.totalSupply().call(),
+  const [ senderAccount, receiverAccount ] = accounts
+  const [ senderAccountBalance, receiverAccountBalance ] = await Promise.all([
+    web3.eth.getBalance(senderAccount),
+    web3.eth.getBalance(receiverAccount),
   ])
-  console.log({ name, symbol, totalSupply })
-})()
+  console.log({ senderAccount, senderAccountBalance: web3.utils.fromWei(senderAccountBalance) })
+  console.log({ receiverAccount, receiverAccountBalance: web3.utils.fromWei(receiverAccountBalance) })
+
+  const rawTransaction: TxData = {
+    nonce: await web3.eth.getTransactionCount(senderAccount),
+    gasPrice: 200000000,
+    gasLimit: 21000, // < number will lead to an error
+    to: receiverAccount,
+    value: 100000000000000,
+  }
+  const transaction = new Transaction(rawTransaction)
+  const privateKeySender = '3328bcb7f373ec287b4e15390522a3fd2fb904fd2677796c68329e20651def60'
+  const senderPrivateKeyHex = Buffer.from(privateKeySender, 'hex')
+  transaction.sign(senderPrivateKeyHex)
+
+  const serializedTransaction = transaction.serialize().toString('hex')
+  const transactionReceipt = await web3.eth.sendSignedTransaction(serializedTransaction)
+  console.log({ transactionReceipt })
+})().then().catch(e => console.error(e.message))
 
---
+++ 1.fundamentals/src/remote-bc.ts
@@ -0,0 +1,31 @@
+import Web3 from 'web3'
+
+const mainnetUrl = 'https://mainnet.infura.io/v3/af3df0d221fc422596bd1ddce10fc709'
+// const rinkebyUrl = 'https://rinkeby.infura.io/v3/af3df0d221fc422596bd1ddce10fc709'
+const web3 = new Web3(mainnetUrl)
+
+const mainnetAccount = '0x742d35Cc6634C0532925a3b844Bc454e4438f44e'
+
+// const rinkebyAccount = '0xa7a82DD06901F29aB14AF63faF3358AD101724A8'
+
+;(async () => {
+  console.clear()
+
+  const balance = await web3.eth.getBalance(mainnetAccount)
+  const ethBalance = web3.utils.fromWei(balance, 'ether')
+  console.log({ ethBalance })
+  
+  const transactionCount = await web3.eth.getTransactionCount(mainnetAccount)
+  console.log({ transactionCount })
+
+  const contractAddress = '0x0d8775f648430679a709e98d2b0cb6250d2887ef' // BAT
+  const abi = [{"constant":true,"inputs":[],"name":"batFundDeposit","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"batFund","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"tokenExchangeRate","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"finalize","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"version","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"refund","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"tokenCreationCap","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"isFinalized","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"fundingEndBlock","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"ethFundDeposit","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"createTokens","outputs":[],"payable":true,"type":"function"},{"constant":true,"inputs":[],"name":"tokenCreationMin","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"fundingStartBlock","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"_ethFundDeposit","type":"address"},{"name":"_batFundDeposit","type":"address"},{"name":"_fundingStartBlock","type":"uint256"},{"name":"_fundingEndBlock","type":"uint256"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"LogRefund","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"CreateBAT","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Approval","type":"event"}] as any
+  const contract = new web3.eth.Contract(abi, contractAddress)
+  const [ name, symbol, totalSupply ] = await Promise.all([
+    contract.methods.name().call(),
+    contract.methods.symbol().call(),
+    contract.methods.totalSupply().call(),
+  ])
+  console.log({ name, symbol, totalSupply })
+})()
+
